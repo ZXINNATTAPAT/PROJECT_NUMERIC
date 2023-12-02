446cf0e8250cef15f351a2ef5ccf02d264882f6b
Nattapatscz <bigboyzin88@gmail.com>
2023-12-02 18:15:52 +0700

    Hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 
 function Home() {
-  let imgSrc =require('../img/Proflie.jpg')
+  // let imgSrc =require('../img/Proflie.jpg')
   return (
     <>
                
                <div class="border rounded-4 bg-light position-absolute top-50 start-50 translate-middle w-50 p-3 shadow p-3 mb-5 bg-body rounded" >
-                <img src={imgSrc} alt='' width="150" height="150" class="rounded-circle position-absolute top-0 start-100 translate-middle "></img>
+                {/* <img src={imgSrc} alt='' width="150" height="150" class="rounded-circle position-absolute top-0 start-100 translate-middle "></img> */}
               <h1>HELLO </h1>
               <h2>This is the program that the gods still use.&#128054;</h2>
               <p><center>Calculater Nummerical</center></p>
